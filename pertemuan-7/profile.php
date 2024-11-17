82ef282ce5ccb055231477ec2197a1e391c9f747
Ananda Dwi Nurrohman <if23.anandanurrohman@mhs.ubpkarawang.ac.id>
2024-11-17 13:29:42 +0700

    Add files via upload

---
+++ pertemuan-7/profile.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+if($_SESSION['role'] ≠ 'user') {
+    session_destroy();
+    header('Location:index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Selamat datang admin</h1>
+    <a href="./backend/logout.php"</a>
+</body>
+</html>
